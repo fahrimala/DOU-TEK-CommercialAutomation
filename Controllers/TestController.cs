62a75906ac6a4098c3194e582d14bbf0f254f3f1
Fahri MALA <43324931+fahrimala@users.noreply.github.com>
2024-09-11 10:45:39 +0300

    DOU-TEK-CommercialAutomation
    Doğuş Üniversitesi Final Projem

---
+++ Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(Personel p)
+        {
+            if (Request.Files.Count > 0)
+            {
+                string dosyaadi = Path.GetFileName(Request.Files[0].FileName);
+                string uzanti = Path.GetExtension(Request.Files[0].FileName);
+                string yol = "~/Image/" + dosyaadi + uzanti;
+                Request.Files[0].SaveAs(Server.MapPath(yol));
+                p.PersonelGorsel = "/Image/" + dosyaadi + uzanti;
+            }
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Controllers/resimController.cs
@@ -0,0 +1,47 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class resimController : Controller
+    {
+        // GET: resim
+        Context c = new Context();
+        public ActionResult ResimYukle()
+        {
+            return View();
+        }
+        public ActionResult ResimYukle(HttpPostedFileBase httpPostedFileBase)
+        {
+            if (httpPostedFileBase.ContentLength > 0)
+            {
+                string isim = Path.GetFileName(httpPostedFileBase.FileName);
+                string yol = Path.Combine(Server.MapPath("~/Image/"), isim);
+                httpPostedFileBase.SaveAs(yol);
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Res2()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Res2(Personel p)
+        {
+            //string isim = Path.GetFileName(p.ImageFile.FileName);
+            //p.PersonelGorsel = "~/Image/" + isim;
+            //isim = Path.Combine(Server.MapPath("~/Image/"), isim);
+            //p.ImageFile.SaveAs(isim);
+            //c.Personels.Add(p);
+            //c.SaveChanges();
+            return View();
+        }
+    }
+}
